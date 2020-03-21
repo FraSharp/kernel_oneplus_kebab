14cd1ee4dcf16a6b0834eed16fb8c51c482cf7a1
Paolo Valente <paolo.valente@linaro.org>
2020-03-21 10:45:19 +0100

    block, bfq: turn put_queue into release_process_ref in __bfq_bic_change_cgroup
    A bfq_put_queue() may be invoked in __bfq_bic_change_cgroup(). The
    goal of this put is to release a process reference to a bfq_queue. But
    process-reference releases may trigger also some extra operation, and,
    to this goal, are handled through bfq_release_process_ref(). So, turn
    the invocation of bfq_put_queue() into an invocation of
    bfq_release_process_ref().
    
    Tested-by: cki-project@redhat.com
    Signed-off-by: Paolo Valente <paolo.valente@linaro.org>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -2979,7 +2979,6 @@ static void bfq_bfqq_save_state(struct bfq_queue *bfqq)
 		     bfqq->wr_cur_max_time);
 }
 
-static
 void bfq_release_process_ref(struct bfq_data *bfqd, struct bfq_queue *bfqq)
 {
 	/*
