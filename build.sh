87590d8560caa886941f04a58a293610971a79e0
FraSharp <s23265@iisve.it>
2022-09-30 09:13:56 +0200

    build script: hardcode output path
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -4,3 +4,8 @@ export python="python3"
 
 lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out vendor/kona-perf_defconfig -j69
 lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out -j69
+
+rm $HOME/Downloads/s/Image
+rm $HOME/Downloads/s/dtbo.img
+cp ./out/arch/arm64/boot/Image $HOME/Downloads/s
+cp ./out/arch/arm64/boot/dtbo.img $HOME/Downloads/s
