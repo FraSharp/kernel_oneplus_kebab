cd6f96fe66d05bf1dd2f4e185d44e7586a5f1aa3
FraSharp <s23265@iisve.it>
2022-10-04 15:13:06 +0200

    mm: conditionally disable readahead
    Signed-off-by: FraSharp <s23265@iisve.it>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2593,8 +2593,13 @@ int __must_check write_one_page(struct page *page);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
+#ifdef CONFIG_VM_READHEAD_DISABLED
+#define VM_MAX_READAHEAD	0	/* kbytes */
+#define VM_MIN_READAHEAD	0	/* kbytes (includes current page) */
+#else
 #define VM_MAX_READAHEAD	128	/* kbytes */
 #define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#endif
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
 			pgoff_t offset, unsigned long nr_to_read);
