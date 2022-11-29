1f041fd690b17c218ce85f6801ddfa9cd25b7d37
FraSharp <s23265@iisve.it>
2022-11-29 18:54:41 +0100

    build script: update clang, use verbose make output
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
-export CLANG_PATH="/opt/homebrew/Cellar/llvm/14.0.6_1/bin/"
+export CLANG_PATH="/opt/homebrew/opt/llvm/bin/"
 export CROSS_COMPILE="$HOME/src/proton-clang/bin/aarch64-linux-gnu-"
 export python="python3"
 
-lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out vendor/kona-perf_defconfig -j69
-lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out -j69
+lkmake V=1 ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out vendor/kona-perf_defconfig -j69
+lkmake V=1 ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out -j69
 
 rm $HOME/Downloads/s/Image
 rm $HOME/Downloads/s/dtbo.img
