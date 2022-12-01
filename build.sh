6d07a9d2f023a7c7bba9c102081414c871f47bf3
FraSharp <s23265@iisve.it>
2022-12-01 17:11:46 +0100

    build script: don't verbose output
    Signed-off-by: FraSharp <s23265@iisve.it>

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@ export CLANG_PATH="/opt/homebrew/opt/llvm/bin/"
 export CROSS_COMPILE="$HOME/src/proton-clang/bin/aarch64-linux-gnu-"
 export python="python3"
 
-lkmake V=1 ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out vendor/kona-perf_defconfig -j69
-lkmake V=1 ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out -j69
+lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out vendor/kona-perf_defconfig -j69
+lkmake ARCH=arm64 SUBARCH=arm64 LLVM=1 LLVM_IAS=1 O=out -j69
 
 rm $HOME/Downloads/s/Image
 rm $HOME/Downloads/s/dtbo.img
